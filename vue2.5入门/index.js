5e5b396d4f9f4616758aae503ae6c251551916ac
xl <1756625653@qq.com>
2018-11-05 21:46:15 +0800

    update

--- vue2.5入门/index.js
+++ vue2.5入门/index.js
@@ -1,6 +1,27 @@
 var app=new Vue({
     el:"#app",
+    //template:"<h1>挂载点模板</h1>",
     data:{
-        msg:"Hello Vue"
+        msg:"Hello Vue",
+        content:"this is content",
+        firstName:"",
+        lastName:"",
+        count:0
+    },
+    computed:{
+        fullName:function(){
+            return this.firstName+this.lastName;
+        }
+    },
+    watch:{
+        // firstName:function(){
+        //         this.count++;
+        // },
+        // lastName:function(){
+        //         this.count++;
+        // }
+        fullName:function(){
+            this.count++;
+        }
     }
-})
+});
