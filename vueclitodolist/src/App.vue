9af92ca588ff676eba171b27d4bfcbb365041875
xl <1756625653@qq.com>
2018-11-10 17:16:01 +0800

    update

--- vueclitodolist/src/App.vue
+++ vueclitodolist/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
     <h1>ToDoList</h1>
-    <input v-model="content"/>
+    <input v-model="content" @keydown.enter="handleClick"/>
     <button @click="handleClick">提交</button>
     <ul>
-      <li v-for="(item,index) in items" :key="index"></li>
+      <todo-item v-for="(item, index) in items" :key="index" :content="item" :index="index"
+      @delete="handleDelete"></todo-item>
     </ul>
   </div>
 </template>
 
 <script>
+import TodoItem from './components/TodoItem'
 export default {
   data () {
     return {
@@ -17,10 +19,18 @@ export default {
       items: []
     }
   },
+  components: {
+    'todo-item': TodoItem
+  },
   methods: {
-    handleCllick () {
-      this,items.push(this.content);
-      this.content='';
+    handleClick () {
+      if (this.content){
+        this.items.push(this.content)
+      }
+      this.content = ''
+    },
+    handleDelete (index) {
+      this.items.splice(index, 1)
     }
   }
 }
---
+++ vueclitodolist/src/components/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <li @click="handleClick">{{content}}</li>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['content', 'index'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
