6a78612419f8317008c7c58ee63f0b9a6466a1fc
xl <1756625653@qq.com>
2018-11-10 16:55:35 +0800

    update

---
+++ vueclitodolist/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>ToDoList</h1>
+    <input v-model="content"/>
+    <button @click="handleClick">提交</button>
+    <ul>
+      <li v-for="(item,index) in items" :key="index"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      content: '',
+      items: []
+    }
+  },
+  methods: {
+    handleCllick () {
+      this,items.push(this.content);
+      this.content='';
+    }
+  }
+}
+</script>
+
+<style>
+</style>
