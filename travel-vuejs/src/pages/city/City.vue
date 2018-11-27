e4b94a999ef444fb359b99ea18c5db1fb852ef81
ty2 <1756625653@qq.com>
2018-11-27 11:33:21 +0800

    update

--- travel-vuejs/src/pages/city/City.vue
+++ travel-vuejs/src/pages/city/City.vue
@@ -33,7 +33,6 @@ export default {
         .then(this.handleGetCityInfoSucc)
     },
     handleGetCityInfoSucc (res) {
-      console.log(res)
       res = res.data
       if (res.ret && res.data) {
         const data = res.data
