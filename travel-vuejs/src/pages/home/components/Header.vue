cc81bf68aace2a7435a13b6b097fe763e6106f20
ty2 <1756625653@qq.com>
2018-11-27 09:58:30 +0800

    update

--- travel-vuejs/src/pages/home/components/Header.vue
+++ travel-vuejs/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输出城市/景点/游玩
   </div>
+  <router-link to='/city'>
   <div class="header-right">
       {{this.city}}
       <span class="iconfont arrow-icon">&#xe64a;</span>
   </div>
+  </router-link>
 </div>
 </template>
 
@@ -32,7 +34,7 @@ export default {
   @import "~styles/varibles.styl"
   .header
     display: flex
-    line-height: .86rem
+    line-height: $headerHeight
     background: $bgColor
     color: #fff
     .header-left
@@ -52,6 +54,7 @@ export default {
       border-radius: .1rem
       color: #ccc
     .header-right
+      color: #fff
       width: 1.24rem
       float: right
       text-align: center
